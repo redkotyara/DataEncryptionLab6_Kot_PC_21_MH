4cc306e338f5041c20dc67474110653109fc98f3
Maksym Kot <redkotyara@yahoo.com>
2022-11-03 23:20:06 +0200

    Initial commit

---
+++ DataEncryptionLab6_Kot_PC_21_MH/Form1.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DataEncryptionLab6_Kot_PC_21_MH
+{
+  public partial class Form1 : Form
+  {
+    public Form1()
+    {
+      InitializeComponent();
+    }
+  }
+}
